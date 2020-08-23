c0998cf8482513852c412d41a1805d736d24f6f7
Divyagb29 <divya2229mgb@gmail.com>
2020-08-23 18:33:01 +0530

    player starts from zero

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,13 @@
 public class SnakeLadder
 {
+   public static void fromPositionZero()
+   {
+     System.out.println("Player Start from Zero");
+   }
    public static void main(String ag[])
    {
 	   System.out.println("	welcome to the Snake & Ladder Game");
+	   SnakeLadder sl = new SnakeLadder();
+	   sl.fromPositionZero();
    }
 }
