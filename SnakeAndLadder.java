5211a4a3287d420e7b39fc8efb5708a7efec33df
Divyagb29 <divya2229mgb@gmail.com>
2020-08-23 19:06:38 +0530

    check player steps into ladder/snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -10,11 +10,28 @@ public class SnakeLadder
 	    int RandonNo1To6 = random.nextInt(6)+1;
 	    System.out.println("random number is "+RandonNo1To6);
    }
+   public static void CheckSnake_Ladder()
+	{ 
+         int RandonNo1To6=getRandomNum();
+         Random random = new Random();
+         int RandomCheck=random.nextInt(3);
+         	if(RandomCheck == 2)
+			{
+				System.out.println("oops you step on snake you will got 2 step back");
+			}
+			else if(RandomCheck == 0)
+			{
+				System.out.println("you made a foult so you will be in same position");
+			}
+			else
+				System.out.println("congrats you have incremented by "+RandonNo1To6);
+
+        }
    public static void main(String ag[])
    {
 	   System.out.println("	welcome to the Snake & Ladder Game");
-	   SnakeLadder sl = new SnakeLadder();
-	   sl.fromPositionZero();
-	   sl.getRandomNum();
+	   fromPositionZero();
+	   System.out.println(getRandomNum());
+	   CheckSnake_Ladder();
    }
 }
