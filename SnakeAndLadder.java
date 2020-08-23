9ab32ddf7f7faa1b6e19fe91646a9f23867c299e
Divyagb29 <divya2229mgb@gmail.com>
2020-08-23 18:45:41 +0530

    getting random numbers between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,10 +4,17 @@ public class SnakeLadder
    {
      System.out.println("Player Start from Zero");
    }
+   public static void getRandomNum()
+   {
+        Random random = new Random();
+	    int RandonNo1To6 = random.nextInt(6)+1;
+	    System.out.println("random number is "+RandonNo1To6);
+   }
    public static void main(String ag[])
    {
 	   System.out.println("	welcome to the Snake & Ladder Game");
 	   SnakeLadder sl = new SnakeLadder();
 	   sl.fromPositionZero();
+	   sl.getRandomNum();
    }
 }
